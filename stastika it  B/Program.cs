8acc406a29b2bf4eecc1348b1cce420394ef0f01
slavicek.martin <slaviekmartin@post.cz>
2021-11-23 10:15:26 +0100

    Přidejte soubory projektu.

---
+++ stastika it  B/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace stastika_it__B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Statistika IT1 B!");
+
+        }
+    }
+}
