69c81ad7b92d0e7e8857437a2d26429cc611317f
slavicek.martin <slaviekmartin@post.cz>
2021-11-23 11:35:06 +0100

    ja

--- stastika it  B/Program.cs
+++ stastika it  B/Program.cs
@@ -6,8 +6,35 @@ namespace stastika_it__B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Statistika IT1 B!");
-
+            ConsoleKeyInfo keyInfo;
+            string povoZnaky = "0123456789";
+            string text = "";
+            int soucet = 0;
+            int pocet = 0; 
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                if (povoZnaky.Contains(keyInfo.KeyChar))
+                {
+                    //zde se program dostane při stisku čísla
+                    text += keyInfo.KeyChar;
+                    Console.Write(keyInfo.KeyChar);
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    if(text != "")
+                    {
+                       soucet 
+                    }
+                    Console.WriteLine();
+                    text = "";
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace && text.Length > 0)
+                {
+                    Console.Write("\b \b");
+                     text = text.Substring(0, text.Length - 1);
+                }
+            } while (keyInfo.Key != ConsoleKey.Escape);          
         }
     }
 }
